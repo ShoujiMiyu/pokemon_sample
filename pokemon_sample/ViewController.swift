b232e52ff2425407dace2d67c7d766bbc1ed4d57
shoujimiyu <>
2024-02-02 12:50:20 +0900

    first commit

---
+++ pokemon_sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokemon_sample
+//
+//  Created by 荘司実祐 on 2024/02/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
